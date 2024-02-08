dc226a2ebbbfe3d510384f26c0be4796806df7d3
Chanagun Viriyasathapornpong <chanagun.vir@gmail.com>
2024-02-09 01:29:38 +0700

    09/02/2024 01:29AM
    REST API With MongoDB

--- VacQ/controllers/hospitals.js
+++ VacQ/controllers/hospitals.js
@@ -1,34 +1,82 @@
+const Hospital = require('../models/Hospital');
+
 //@desc     Get all hospitals
 //@route    GET /api/v1/hospitals
 //@access   Public
-exports.getHospitals=(req,res,next)=>{
-    res.status(200).json({success:true , msg:"Get all hospitals"});
+exports.getHospitals=async (req,res,next)=>{
+    try {
+        const hospitals = await Hospital.find();
+        res.status(200).json({success:true, count:hospitals.length, data:hospitals});
+    } catch (err) {
+        res.status(400).json({success:false});
+    }
+    // res.status(200).json({success:true , msg:"Get all hospitals"});
 };
 
 //@desc     Get single hospital
 //@route    GET /api/v1/hospitals/:id
-//@access   Public
-exports.getHospital=(req,res,next)=>{
-    res.status(200).json({success:true, msg:`Get hospital ${req.params.id}`});
+//@access   Public 
+exports.getHospital=async (req,res,next)=>{
+    try {
+        const hospital = await Hospital.findById(req.params.id);
+
+        if (!hospital) {
+            return res.status(400).json({success:false});
+        }
+    } catch (err) {
+        res.status(400).json({success:false});
+    }
+    // res.status(200).json({success:true, msg:`Get hospital ${req.params.id}`});
 };
 
 //@desc     Create new hospital
 //@route    POST /api/v1/hospitals
 //@access   Private
-exports.createHospital=(req,res,next)=>{
-    res.status(200).json({success:true , msg:"Create new hospitals"});
+exports.createHospital= async (req,res,next)=>{
+    console.log(req.body);
+    const hospital = await Hospital.create(req.body);
+    res.status(201).json({
+        success: true,
+        data: hospital
+    });
+    // console.log(req.body)
+    // res.status(200).json({success:true , msg:"Create new hospitals"});
 };
 
 //@desc     Update hospital
 //@route    PUT /api/v1/hospitals
 //@access   Private
-exports.updateHospital=(req,res,next)=>{
-    res.status(200).json({success:true, msg:`Update hospital ${req.params.id}`});
+exports.updateHospital=async (req,res,next)=>{
+    try {
+        const hospital = await Hospital.findByIdAndUpdate(req,params.idm, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!hospital){
+            return res.status(400).json({success:false});
+        }
+        res.status(200).json({success:true, data:hospital});
+    } catch (err) {
+        res.status(400).json({success:false});
+    }
+    // res.status(200).json({success:true, msg:`Update hospital ${req.params.id}`});
 };
 
 //@desc     Delete hospital
 //@route    DELETE /api/v1/hospitals/:id
 //@access   Private
-exports.deleteHospital=(req,res,next)=>{
-    res.status(200).json({success:true, msg:`Delete hospital ${req.params.id}`});
+exports.deleteHospital=async (req,res,next)=>{
+    try {
+        const hospital = await Hospital.findByIdAndDelete(req.params.id);
+
+        if (!hospital){
+            return res.status(400).json({success:false});
+        }
+
+        res.status(200).json({success:true, data: {}});
+    } catch (err) {
+        res.status(400).json({success:false});
+    }
+    // res.status(200).json({success:true, msg:`Delete hospital ${req.params.id}`});
 };
--- VacQ/server.js
+++ VacQ/server.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
-//Route files
+// Route files
 const hospitals = require('./routes/hospitals');
 
-//Load env vars
-dotenv.config({path:'./config/config.env'});
+
+// Load env vars
+dotenv.config({path: './config/config.env'});
+
+// Connect to database
+connectDB();
 
 const app = express();
 
-//Mount routers
-app.use('/api/v1/hospitals',hospitals);
+// Body parser
+app.use(express.json());
 
-//PORT in ENV ur 5000 (if u forgot set it)
-const PORT = process.env.PORT || 5000;
-app.listen(PORT,console.log("Server running in",process.env.NODE_ENV,"mode on port",PORT));
+app.use('/api/v1/hospitals', hospitals)
+
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(PORT, console.log("Server running in", process.env.NODE_ENV, 'mode on port', PORT));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+
+    // Close server & exit process
+    server.close(() => process.exit(1))
+})
