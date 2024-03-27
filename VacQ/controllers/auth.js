81ddeaaa1f3ddfa4af7e27254d462902ebfb6a79
Chanagun Viriyasathapornpong <chanagun.vir@gmail.com>
2024-03-28 05:52:38 +0700

    05:52AM 28/03/2024
    Frontend

--- VacQ/controllers/auth.js
+++ VacQ/controllers/auth.js
@@ -68,8 +68,13 @@ const sendTokenResponse = (user, statusCode, res) => {
         options.secure = true;
     }   
 
-    res.status(statusCode).cookie('token',token,options).json({
+    res.status(statusCode)/*.cookie('token',token,options)*/.json({
         success: true,
+        //add for frontend
+        _id:user._id,
+        name: user.name,
+        email:user.email,
+        //end for frontend
         token
     })
 }
