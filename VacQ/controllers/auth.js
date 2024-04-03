01dec7bcab1ddbee4a0f83c8be8b3d41b73cd6df
Chanagun Viriyasathapornpong <chanagun.vir@gmail.com>
2024-04-03 15:08:44 +0700

    03/04/2024 03:08PM
    API Security

--- VacQ/controllers/auth.js
+++ VacQ/controllers/auth.js
@@ -36,6 +36,11 @@ exports.login =async (req,res,next) => {
         return res.status(400).json({success:false, msg: 'Please provide an email and password'});
     }
 
+    //Type of email & password
+    if (typeof email != 'string' || typeof password != 'string') {
+        return res.status(400).json({success:false, msg: 'Cannot convert email and password to string'});
+    }
+
     //Check for user 
     const user = await User.findOne({email}).select('+password');
     if (!user) {
@@ -85,4 +90,19 @@ const sendTokenResponse = (user, statusCode, res) => {
 exports.getMe = async (req,res,next) => {
     const user = await User.findById(req.user.id);
     res.status(200).json({success: true, data: user});
+}
+
+//@desc Log user out / clear cookie
+//@route GET /api/v1/auth/logout
+//@access Private 
+exports.logout  = async(req,res,next) => {
+    res.cookie('token','none', {
+        expires: new Date(Date.now() + 10*1000),
+        httpOnly: true
+    });
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
 }
